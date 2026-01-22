201b5da6c6b687cde85413487adeb2f455befb77
Ates Balci <atesbalci@gmail.com>
2026-01-22 14:03:58 +0200

    Godot 4.5.1 update

--- Sources/GDExtension/include/libgodot.h
+++ Sources/GDExtension/include/libgodot.h
@@ -31,11 +31,26 @@
 #ifndef LIBGODOT_H
 #define LIBGODOT_H
 
+#include <libgodot/gdextension_interface.h>
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "gdextension_interface.h"
+// Export macros for DLL visibility
+#if defined(_MSC_VER) || defined(__MINGW32__)
+#define LIBGODOT_API __declspec(dllexport)
+#elif defined(__GNUC__) || defined(__clang__)
+#define LIBGODOT_API __attribute__((visibility("default")))
+#endif // if defined(_MSC_VER)
+
+typedef void *CallbackData;
+typedef void *ExecutorData;
+typedef void (*InvokeCallback)(CallbackData p_data);
+typedef void (*InvokeCallbackFunction)(InvokeCallback p_callback, CallbackData p_callback_data, ExecutorData p_executor_data);
+typedef void *LogCallbackData;
+typedef void (*LogCallbackFunction)(LogCallbackData p_data, const char *p_log_message, bool p_err);
 
 /**
  * @name libgodot_create_godot_instance
@@ -46,10 +61,11 @@ extern "C" {
  * @param p_argc The number of command line arguments.
  * @param p_argv The C-style array of command line arguments.
  * @param p_init_func GDExtension initialization function of the host application.
+ * @param p_log_func Initialization log function, called with log message c string.
  *
  * @return A pointer to created \ref GodotInstance GDExtension object or nullptr if there was an error.
  */
-GDExtensionObjectPtr libgodot_create_godot_instance(int p_argc, char *p_argv[], GDExtensionInitializationFunction p_init_func);
+LIBGODOT_API GDExtensionObjectPtr libgodot_create_godot_instance(int p_argc, char *p_argv[], GDExtensionInitializationFunction p_init_func, InvokeCallbackFunction p_async_func, ExecutorData p_async_data, InvokeCallbackFunction p_sync_func, ExecutorData p_sync_data, LogCallbackFunction p_log_func, LogCallbackData p_log_data);
 
 /**
  * @name libgodot_destroy_godot_instance
@@ -60,7 +76,7 @@ GDExtensionObjectPtr libgodot_create_godot_instance(int p_argc, char *p_argv[], 
  * @param p_godot_instance The reference to the GodotInstance object to destroy.
  *
  */
-void libgodot_destroy_godot_instance(GDExtensionObjectPtr p_godot_instance);
+LIBGODOT_API void libgodot_destroy_godot_instance(GDExtensionObjectPtr p_godot_instance);
 
 #ifdef __cplusplus
 }
